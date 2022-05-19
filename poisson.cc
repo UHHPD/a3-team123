c0dadfd7f42d9167ce4a1d393c1b2d121354faa9
Bennet <103441059+Bennetfhh@users.noreply.github.com>
2022-05-19 11:07:13 +0000

    s

--- poisson.cc
+++ poisson.cc
@@ -1,20 +1,32 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
+using namespace std;
 
-
-double poisson(double mu, int k) {
-    return 0;
-}
 
 int main() {
-    using namespace std;
-
-
+    ofstream fou2("histpoi.txt")
+    ofstream fou("hist.txt");
+    vector<int> zaehler(11);
     ifstream fin("datensumme.txt");
-    int n_i;
+    int n_i = 0;
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> n_i;
+        if(n_i < 11){
+              zaehler[n_i] += 1;
+        }
+      }
+  fin.close();
+  for ( unsigned int k = 0 ; k < zaehler . size () ; ++ k ) {
+  fou << k << " " << zaehler[k] << std::endl;
+  std::cout << k << ":" << zaehler [ k ] << std::endl ;
+  for(int i = 0; i < k , i++){
+      fou2 >> poisson(3.11538, k);
     }
-    fin.close();
+}
+double poisson(double mu, int k)
+{
+  double pw = (pow(mu,k) * exp(-mu))/tgamma(k);
+  return pw;
 }
